0abf3c3dc03da3514b302723303b4882be13f77c
Simon Esders <simonesders2003@gmail.com>
2024-01-21 00:11:12 +0100

    first commit

--- chat/views.py
+++ chat/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {'username': 'Penis'})
+    if request.method == 'POST':
+        print('Recived data')
+    return render(request, 'index.html', {'username': request.user})
