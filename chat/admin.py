48a2e9cd04f872a9e66efb55f90161d931c99dde
Simon Esders <simonesders2003@gmail.com>
2024-01-21 00:00:28 +0100

    added classes

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from chat.models import Chat, Message
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text', 'created_at', 'chat', 'author', 'receiver')
+    list_display = ('text', 'created_at', 'chat', 'author', 'receiver')
+
 # Register your models here.
+admin.site.register(Chat)
+admin.site.register(Message, MessageAdmin)
