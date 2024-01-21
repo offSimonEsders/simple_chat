3a44836b25067f01ad874e6ff1272a2e90fa6abd
Simon Esders <simonesders2003@gmail.com>
2024-01-21 01:36:44 +0100

    login and show messages

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from chat.models import Message, Chat
+
+
 # Create your views here.
 
 def index(request):
     if request.method == 'POST':
-        print('Recived data')
-    return render(request, 'index.html', {'username': request.user})
+        print(f'Recived data {request.POST["message"]}')
+        Message.objects.create(text=request.POST["message"], chat=Chat.objects.get(id=2), author=request.user, receiver=request.user)
+    chatmessages = Message.objects.filter(chat__id=2)
+    return render(request, 'index.html', {'messages': chatmessages})
+
+def auth_view(request):
+    return render(request, 'auth.html')
